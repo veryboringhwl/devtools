9582db5569c556162781c7140175ba13a3eebd33
veryboringhwl <d.linshengjie@gmail.com>
2026-05-09 18:10:39 +0100

    e

---
+++ src/devtools/devtools.ts
@@ -0,0 +1,24 @@
+let panelWindow: Window | null = null;
+
+chrome.devtools.panels.create("Mapped Elements", "", "panel.html", (panel) => {
+  panel.onShown.addListener((pw) => {
+    panelWindow = pw;
+  });
+
+  panel.onHidden.addListener(() => {
+    panelWindow = null;
+  });
+
+  panel.onSearch.addListener((action, queryString) => {
+    if (panelWindow) {
+      panelWindow.postMessage(
+        {
+          source: "devtools-command",
+          action,
+          query: queryString || "",
+        },
+        "*",
+      );
+    }
+  });
+});
